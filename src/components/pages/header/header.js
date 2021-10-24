c2d9fe233f31e51e52065183c5f65f657337348c
Suvadeep <maityamalendu18@gmail.com>
2021-10-24 15:22:37 +0530

    made the landing page of the website

---
+++ src/components/pages/header/header.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+import { Anchor, Drawer, Button } from "antd";
+
+import './header.css';
+const { Link } = Anchor;
+function AppHeader() {
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  return (
+    <div className="container-fluid">
+     
+      <div className="header">
+        <div className="logo">
+          <img
+            src="https://raw.githubusercontent.com/winterofcode/winterofcode2020/master/src/Assets/org-logo.png"
+            style={{ height: 40, width: 40 }}
+            alt=""
+          />
+          <a href="https://winterofcode.com">Winter Of Code</a>
+        </div>
+        <div className="mobileHidden">
+          <Anchor targetOffset="65">
+            <Link href="https://dscnsec.com" title="DSC NSEC" />
+            <Link href="#about" title="About" />
+            <Link href="#organisations" title="Organisations" />
+            <Link href="#organisers" title="Organisers" />
+            <Link href="#faq" title="FAQ" />
+            <Link href="#reports" title="Student Reports" />
+            <Link href="#contact" title="Contact" />
+          </Anchor>
+        </div>
+        <div className="mobileVisible">
+          <Button type="primary" onClick={showDrawer}>
+            <i className="fas fa-bars"></i>
+          </Button>
+          <Drawer
+            placement="right"
+            closable={false}
+            onClose={onClose}
+            visible={visible}
+          >
+            <Anchor targetOffset="65">
+              <Link href="https://dscnsec.com" title="DSC NSEC" />
+              <Link href="#about" title="About" />
+              <Link href="#organisations" title="Organisations" />
+              <Link href="#organisers" title="Organisers" />
+              <Link href="#faq" title="FAQ" />
+              <Link href="#reports" title="Student Reports" />
+              <Link href="#contact" title="Contact" />
+            </Anchor>
+          </Drawer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AppHeader;
