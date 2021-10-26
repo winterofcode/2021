c7cb8d304ab228630e1d0e89ef93186461f9623e
Suvadeep <maityamalendu18@gmail.com>
2021-10-26 11:18:05 +0530

    change folder structure and improve media query

--- src/components/pages/home/homepage.js
+++ src/components/pages/home/homepage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "antd";
 import { Carousel } from "antd";
+import logo from "../../../assets/images/logo.jpg";
 const items = [
   {
     key: "1",
@@ -29,24 +30,29 @@ function AppHero() {
   return (
     <div id="hero" className="heroBlock">
       <Carousel>
+
         {items.map((item) => {
           return (
             <div key={item.key} className="container-fluid">
-              <div className="content" >
+              <div className="content">
                 <h3 data-aos="fade-right">{item.title}</h3>
                 <p data-aos="fade-left">{item.content}</p>
                 <div className="btnHolder">
-                  <Button type="primary" size="large"data-aos="zoom-in" >
+                  <Button type="primary" size="large" data-aos="zoom-in">
                     Get Started
                   </Button>
                   <Button size="large">
                     <i className="fas fa-video"></i> Watch a Demo
                   </Button>
                 </div>
+                 {/* <img className="con" style={{ height:400,width:500}} src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" /> */}
               </div>
+              
+              
             </div>
           );
         })}
+
       </Carousel>
     </div>
   );
