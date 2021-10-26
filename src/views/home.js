2be419a0b9e6f64bece32554233b089dae769aa7
Suvadeep <maityamalendu18@gmail.com>
2021-10-26 15:31:42 +0530

    add organisation page and change css

--- src/views/home.js
+++ src/views/home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import AppFaq from "../components/Faq/Faq";
 import AppHero from "../components/home/homepage";
-import './home.css'
+import Project from "../components/Basics/Project";
+import "./home.css";
 function AppHome() {
   return (
     <div className="main">
       <AppHero />
-      <AppFaq/>
-
+      <Project />
+      <AppFaq />
     </div>
   );
 }
