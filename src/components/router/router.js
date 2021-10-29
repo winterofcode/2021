229ffb40e72a75ab42f02ccc00865cabc390744e
Piyush Thakur <piyushthakur3769@gmail.com>
2021-10-29 07:32:28 +0530

    added initial timeline

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "../navbar/navbar";
 import LandingPage from '../landingpage/landingpage';
+import Timeline from "../timeline/timeline";
 
 export default function AppRouter() {
   return (
@@ -13,6 +14,7 @@ export default function AppRouter() {
     // </Router>
     <Router>
       <LandingPage/>
+      <Timeline/>
     </Router>
   );
 }
