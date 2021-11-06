25e21111f00c73d47194b188e639383ccc159cce
Ayush <ayushmishra2854@gmail.com>
2021-11-06 19:26:02 +0530

    semantics of orgs and sponsors page done

--- src/components/orgs/orgs.js
+++ src/components/orgs/orgs.js
@@ -60,7 +60,7 @@ export default function Orgs() {
         return (
             <div>
                 <div className={orgsTitle}>
-                    <h1>Organisations</h1>
+                    <h2>Organisations</h2>
                 </div>
                 <div className={ApplyButton}>
                     <Stack spacing={2} direction="row">
@@ -83,7 +83,7 @@ export default function Orgs() {
             <div>
                 <Parallax>
                 <div className={orgsTitle}>
-                    <h1>Organisations</h1>
+                    <h2>Organisations</h2>
                 </div>
                 <div className={ApplyButton}>
                     <Stack spacing={2} direction="row">
